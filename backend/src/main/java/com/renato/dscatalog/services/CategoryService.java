313d092dbacfdbfde0374eb5b09c71e9d984535c
Jrenatomt <jrenatomt@gmail.com>
2020-09-29 11:35:07 -0300

    h2 database

---
+++ backend/src/main/java/com/renato/dscatalog/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.renato.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.renato.dscatalog.entities.Category;
+import com.renato.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+
+}
