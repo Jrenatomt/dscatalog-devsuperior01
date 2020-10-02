a74da9e19e7984c9cb2210845a6ab4b9905e714c
Jrenatomt <jrenatomt@gmail.com>
2020-10-02 10:09:39 -0300

    DTO

--- backend/src/main/java/com/renato/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/renato/dscatalog/resources/CategoryResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.renato.dscatalog.entities.Category;
+import com.renato.dscatalog.dto.CategoryDTO;
 import com.renato.dscatalog.services.CategoryService;
 
 @RestController
@@ -19,8 +19,8 @@ public class CategoryResource {
 	private CategoryService service;
 
 	@GetMapping
-	public ResponseEntity<List<Category>> findall() {
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findall() {
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
--- backend/src/main/java/com/renato/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/renato/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.renato.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.renato.dscatalog.dto.CategoryDTO;
 import com.renato.dscatalog.entities.Category;
 import com.renato.dscatalog.repositories.CategoryRepository;
 
@@ -16,8 +18,12 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		//List<CategoryDTO> listDto = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		//return listDto;
+		
 	}
 
 }
