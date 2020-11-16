192cb6ded8bcee9ab60dd1314e8b56cd6b5a82f2
Jrenatomt <jrenatomt@gmail.com>
2020-11-16 12:15:34 -0300

    form login

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,21 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import ButtonIcon from '../../../../core/components/Buttonicon';
 import AuthCard from '../Card/Index';
 import './styles.scss'
+import { MakeLogin } from '../../../../core/utils/request';
+import { saveSessionData } from '../../../../core/utils/auth';
+
+type FormData = {
+    username: string;
+    password: string;
+}
 
 const Login = () => {
+
+    const { register, handleSubmit } = useForm<FormData>();
+    const [hasError, setHasError] = useState(false);
+    const history = useHistory();
+
+    const onSubmit = ( data: FormData ) => {
+        MakeLogin(data)
+        .then(response => {
+            setHasError(false);
+            saveSessionData(response.data);
+            history.push('/admin');
+        })
+        .catch( () => {
+            setHasError(true);
+        })
+    }
+
     return (
         <AuthCard title="login">
-            <form className="login-form">
+            {hasError && (<div className="alert alert-danger mt-5 erro-title">
+                usuário ou senha inválido
+            </div>)}
+            <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
                 <input type="email"
                     className="form-control card-base margin-bottom-30"
                     placeholder="Email"
+                    name="username" 
+                    ref={register({ required: true})}
                 />
 
                 <input type="password"
                     className="form-control card-base"
                     placeholder="Senha"
+                    name="password" 
+                    ref={register({ required: true})}
                 />
 
                 <Link to="/admin/auth/recover" className="login-link-recover">
